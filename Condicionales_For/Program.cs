6c78dda5bc09efcfbc188917aa58e6a11cf5c721
Kevin Montaño <Kevin Montao@DESKTOP-IKA0HN7>
2024-03-15 21:59:48 -0500

    ...

--- Condicionales_For/Program.cs
+++ Condicionales_For/Program.cs
@@ -2,7 +2,7 @@
 //Si introduce 1 los números se deben sumar, si introduce 2 los números se deben restar, si es 3 los números se multiplican
 //y si introduce 4 los números se dividen (validar división por cero). Independientemente de la selección, imprima el resultado. 
 //Además, el programa debe tener la posibilidad de volver al menú para pedir 2 nuevos números y hacer la elección de la operación 
-//aritmética o digitar cero para finalizar el programaa.
+//aritmética o digitar cero para finalizar el programaaa.
 
 
 internal class Program
