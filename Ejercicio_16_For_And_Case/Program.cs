611aee9444e4c8de4a049777aefb06e165ae1531
Kevin Montaño <Kevin Montao@DESKTOP-IKA0HN7>
2024-03-18 20:54:38 -0500

    Clase 18-03-2024

---
+++ Ejercicio_16_For_And_Case/Program.cs
@@ -0,0 +1,17 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int acumuladorPar = 0, acumuladorImpar = 0;
+
+        for(int i = 1; i <= 100; i++) 
+        {
+            Console.WriteLine(i);
+            if(i % 2 == 0) acumuladorPar += i;
+            else acumuladorImpar += i;
+          
+        }
+        Console.WriteLine($"La suma de los pares es {acumuladorPar}");
+        Console.WriteLine($"La suma de los impares es {acumuladorImpar}");
+    }
+}
